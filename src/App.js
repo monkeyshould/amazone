35020c227f57cc285570fabf3bfd3d8084c4b381
Jb Laurans <monkeyshould@gmail.com>
2020-12-05 01:51:44 +0100

    routing + CHeckout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header"
+import Checkout from "./Checkout"
+
 function App() {
 return (
-  <div className="app">
+  
     <Router>
+      <div className="app">
+
+      <Header/>
+
       <Switch>
-      <Route path="/">
-  <Header />
-  <Home />
-</Route>
+
+      <Route path="/checkout">
+          <Checkout/>
+        </Route>
+
         <Route path="/">
           <Home />
         </Route>
 
-        
       </Switch>
+      </div>
     </Router>
-  </div>
+  
 );
 }
 export default App;
